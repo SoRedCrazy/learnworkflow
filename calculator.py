acf9b364985fd5c56139c5e842f58ec926b57957
Julien boisgard <julien.boisgard37@gmail.com>
2024-02-22 10:19:25 +0100

    modif test

--- calculator.py
+++ calculator.py
@@ -36,7 +36,7 @@ class TestCalculator(unittest.TestCase):
   def test_divide(self):
     self.assertEqual(self.calculator.divide(10,2), 5)
   def test_divide_false(self):
-    self.assertEqual(self.calculator.divide(10,2), 6)
+    self.assertEqual(self.calculator.divide(10,2), 5)
 # Executing the tests in the above test case class
 if __name__ == "__main__":
   unittest.main()
